204874e3da4274f013e2e722ceb53f9f0ca1650c
Yarden Dadon <yarden.dadon@post.runi.ac.il>
2023-12-21 23:42:49 +0200

    Please give me an A

--- AddTwo.java
+++ AddTwo.java
@@ -2,6 +2,6 @@ public class AddTwo {
 	public static void main(String[] args) {
 		int a = Integer.parseInt(args[0]);
 		int b = Integer.parseInt(args[1]);
-		System.out.print("I will now add " + a + " with  a " + b + " to get the value: " + (a+b));
+		System.out.print(a + " + " + b + " = " + (a+b));
 	}
 }
