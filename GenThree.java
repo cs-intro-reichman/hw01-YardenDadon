6a294e3a1af3f53d11e09d6a1eefacfdac5ca4f2
Yarden Dadon <yarden.dadon@post.runi.ac.il>
2023-12-22 15:35:14 +0200

    Random Numbers done

--- GenThree.java
+++ GenThree.java
@@ -4,11 +4,11 @@
  */
 public class GenThree {
 	public static void main(String[] args) {
-		double a = Integer.parseInt (args[0]);
-		double b = Integer.parseInt (args[1]);
-		double random1 = Math.random() * a;
-		double random2 = Math.random() * a;
-		double random3 = Math.random() * a;
+		int maximum = Integer.parseInt (args[0]);
+		int minimum = Integer.parseInt (args[1]);
+		int random1 = ((int)((Math.random() * (maximum - minimum + 1)) + minimum));
+		int random2 = ((int)((Math.random() * (maximum - minimum + 1)) + minimum));
+		int random3 = ((int)((Math.random() * (maximum - minimum + 1)) + minimum));
 
 		System.out.println(random1);
 		System.out.println(random2);
@@ -17,4 +17,3 @@ public class GenThree {
 		System.out.print("The minimal number generated was " + Math.min(Math.min(random1, random2), random3));
 	}
 }
-multiply by the range
