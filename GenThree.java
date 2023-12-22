1cf8e922a04d93fb058035784d06c161976320fe
Yarden Dadon <yarden.dadon@post.runi.ac.il>
2023-12-22 13:30:02 +0200

    Linear Equatinos completed

--- GenThree.java
+++ GenThree.java
@@ -4,6 +4,17 @@
  */
 public class GenThree {
 	public static void main(String[] args) {
-		// Put your code here	
+		double a = Integer.parseInt (args[0]);
+		double b = Integer.parseInt (args[1]);
+		double random1 = Math.random() * a;
+		double random2 = Math.random() * a;
+		double random3 = Math.random() * a;
+
+		System.out.println(random1);
+		System.out.println(random2);
+		System.out.println(random3);
+
+		System.out.print("The minimal number generated was " + Math.min(Math.min(random1, random2), random3));
 	}
 }
+multiply by the range
